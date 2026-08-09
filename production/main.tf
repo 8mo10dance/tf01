4965278eff90c63316bf4b85e6c0e1992725ac7e
sunda <8mo10dance@protonmail.com>
2026-08-09 10:49:36 +0900

    Import existing production S3 resources

--- production/main.tf
+++ production/main.tf
@@ -13,3 +13,23 @@ provider "aws" {
 module "site" {
   source = "../modules/static-site"
 }
+
+import {
+  to = module.site.aws_s3_bucket.site
+  id = "my-bucket-949926374137-ap-northeast-1-an"
+}
+
+import {
+  to = module.site.aws_s3_bucket_website_configuration.site
+  id = "my-bucket-949926374137-ap-northeast-1-an"
+}
+
+import {
+  to = module.site.aws_s3_bucket_public_access_block.site
+  id = "my-bucket-949926374137-ap-northeast-1-an"
+}
+
+import {
+  to = module.site.aws_s3_bucket_policy.site
+  id = "my-bucket-949926374137-ap-northeast-1-an"
+}
