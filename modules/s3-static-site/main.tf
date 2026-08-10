f48d3971413200ec1a7ef0a146bd0d8e2ea199da
sunda <8mo10dance@protonmail.com>
2026-08-10 19:33:25 +0900

    Restrict production S3 access to CloudFront OAC

--- modules/s3-static-site/main.tf
+++ modules/s3-static-site/main.tf
@@ -15,23 +15,23 @@ resource "aws_s3_bucket_website_configuration" "site" {
 resource "aws_s3_bucket_public_access_block" "site" {
   bucket = aws_s3_bucket.site.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = !var.enable_public_read
+  block_public_policy     = !var.enable_public_read
+  ignore_public_acls      = !var.enable_public_read
+  restrict_public_buckets = !var.enable_public_read
 }
 
 resource "aws_s3_bucket_policy" "site" {
   bucket = aws_s3_bucket.site.id
   policy = jsonencode({
     Statement = concat(
-      [{
+      var.enable_public_read ? [{
         Action    = "s3:GetObject"
         Effect    = "Allow"
         Principal = "*"
         Resource  = "${aws_s3_bucket.site.arn}/*"
         Sid       = "PublicReadGetObject"
-      }],
+      }] : [],
       var.cloudfront_distribution_arn == null ? [] : [{
         Action = "s3:GetObject"
         Condition = {
