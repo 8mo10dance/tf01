779421c34d0c3fd3a31a3911435fe12de87d63c8
sunda <8mo10dance@protonmail.com>
2026-08-15 13:36:27 +0900

    Restrict EC2 HTTP access to ALB

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -66,16 +66,6 @@ resource "aws_security_group" "ec2" {
     self             = false
     to_port          = 22
     }, {
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "Allow HTTP"
-    from_port        = 80
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    protocol         = "tcp"
-    security_groups  = []
-    self             = false
-    to_port          = 80
-    }, {
     cidr_blocks      = []
     description      = ""
     from_port        = 80
